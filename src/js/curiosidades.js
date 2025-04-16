3ff25f09d5df308b4b1301859a30339f0fde28ce
Lucas Dos Reis Aquino <lucas.aquino@jbs.com.br>
2025-04-16 15:13:28 -0300

    [FEAT] Partial imp of curiosities

---
+++ src/js/curiosidades.js
@@ -0,0 +1,15 @@
+function openModal(title, description) {
+    document.getElementById('modal-title').innerText = title;
+    document.getElementById('modal-description').innerText = description;
+    document.getElementById('modal').style.display = 'flex';
+  }
+
+  document.querySelector('.close-button').addEventListener('click', function () {
+    document.getElementById('modal').style.display = 'none';
+  });
+
+  window.addEventListener('click', function (e) {
+    if (e.target.id === 'modal') {
+      document.getElementById('modal').style.display = 'none';
+    }
+});
